0aaec6cfdef50fb14e215317244d9b18d5d32649
Somewhat-average <Matthewraulerson7@gmail.com>
2023-06-11 00:13:36 -0400

    better rng and allows for reusing a deck

--- deck.cpp
+++ deck.cpp
@@ -16,16 +16,29 @@ Deck::Deck() {
 	        deck.push_back(Card(suit, rank, value));
 	    }
 	}
-	//shuffleDeck();
+	currentIndex = 0;
+	shuffleDeck();
 }
 
 void Deck::shuffleDeck() {
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    shuffle(deck.begin(), deck.end(), std::default_random_engine(seed));
+    // Create a PCG engine and seed it with a combination of random device and chrono
+    std::random_device rd;
+    auto seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+    std::seed_seq seedSequence{ rd(), static_cast<unsigned int>(seed & 0xffffffff), static_cast<unsigned int>(seed >> 32) };
+    pcg32 engine(seedSequence);
+    
+    // Shuffle the elements in the cards_ container using the random engine
+    std::shuffle(deck.begin(), deck.end(), engine);
 }
 
 Card Deck::drawCard() {
-    Card card = deck.back();
-    deck.pop_back();
+    if (currentIndex >= deck.size()) {
+        // Reshuffle the deck if currentIndex exceeds the number of cards in the deck
+        shuffleDeck();  // Reshuffle the deck
+        currentIndex = 0;  // Reset the currentIndex to 0
+    }
+
+    Card card = deck[currentIndex];
+    currentIndex++;
     return card;
 }
--- deck.h
+++ deck.h
@@ -3,11 +3,14 @@
 #include <vector>
 #include <chrono>
 #include <random>
+#include "pcg_random.hpp"
 #include <algorithm>
 #include "card.h"
 
+
 class Deck {
 private:
+    size_t currentIndex;
     std::vector<Card> deck;
 
 public:
